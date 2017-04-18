f66199f605cbbb38f2a417959d4cdb77eebde8a1
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2017-04-18 14:43:48 +0300

    Improve run-services.sh
    Proper handling services with spaces in name
    Do not invoke chmod if service file is executable

--- run-services.sh
+++ run-services.sh
@@ -4,12 +4,12 @@ echo '/run-services.sh'
 
 for entry in /services/*.sh
 do
-  echo $entry
-  if [ -f $entry ]; then
-      chmod +x $entry; sync;
-      $entry
+  echo "$entry"
+  if [[ -f "$entry" ]]; then
+      [[ ! -x "$entry" ]] && (chmod +x "$entry"; sync)
+      "$entry"
   fi
 done
 
 echo '/run-agent.sh'
-exec /run-agent.sh
+exec '/run-server.sh'
