0194f39441f1234c9b868c62e992eed19c90e352
Dmitry Tretyakov <dtretyakov@gmail.com>
2017-09-14 15:56:03 +0200

    Add windows images
    Issue: TW-47392

---
+++ build.ps1
@@ -0,0 +1,2 @@
+docker build -t teamcity-minimal-agent:latest-windowsservercore -f windowsservercore/Dockerfile .
+docker build -t teamcity-minimal-agent:latest-nanoserver -f nanoserver/Dockerfile .
