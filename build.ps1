b0d87ea6df61ef225f578ecc2270748e6bc1d140
Dmitry Tretyakov <dtretyakov@gmail.com>
2018-07-10 12:29:07 +0200

    Support windows 1803 containers
    Issue: TW-55697

--- build.ps1
+++ build.ps1
@@ -1,18 +1,13 @@
-# 1709 images
-docker build `
- -t teamcity-minimal-agent:latest-windowsservercore-1709 `
- -f windowsservercore-1709/Dockerfile .
+# Build semi annual images
+$tags = @("1803")
+foreach ($tag in $tags) {
+ docker build `
+ --build-arg TAG=$tag `
+ -t "teamcity-minimal-agent:latest-nanoserver-$tag" `
+ -f nanoserver/Dockerfile .
+}
 
-docker build `
- -t teamcity-minimal-agent:latest-nanoserver-1709 `
- -f nanoserver-1709/Dockerfile .
-
-# old images
-docker build --isolation=hyperv `
- -t teamcity-minimal-agent:latest-windowsservercore `
- -t teamcity-minimal-agent:latest-windowsservercore-ltsc2016 `
- -f windowsservercore-ltsc2016/Dockerfile .
-
+# Build images with long term support
 docker build --isolation=hyperv `
  -t teamcity-minimal-agent:latest-nanoserver `
  -t teamcity-minimal-agent:latest-nanoserver-sac2016 `
